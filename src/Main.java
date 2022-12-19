84d6639a5cebf1a71d4245b04b1a8f9ec36a0505
semenikhin <garry@nextmail.ru>
2022-12-19 19:01:08 +0300

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -42,5 +42,11 @@ public class Main {
         frog = frog / 3.5;
         System.out.println(frog);
 
+
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес " + totalWeight + " кг.");
+
     }
 }
